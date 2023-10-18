6de11890ae88e39a138a3985567447b5c6d4cea4
dimasliva <janzerikYT@yandex.ru>
2023-10-18 17:02:37 +0500

    change .json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  base: '/react_effector/',
-  root: './',
-  build: {
-    outDir: 'build',
-  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
